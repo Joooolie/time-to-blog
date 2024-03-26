16741ed3ac23d068ee858e2b920a70670fa76694
Julie Zenou <me@juliezen.com>
2024-03-26 23:53:40 +1100

    set up page for a new post

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/new-post", (req, res) => {
+    res.render("new-post.ejs");
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
